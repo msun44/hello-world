8db189cc34e9c469b031cb041a0ca1637a523731
msun44 <79740548+msun44@users.noreply.github.com>
2021-02-27 15:53:08 +0800

    add name and edit addNumber()
    add name to line 2
    modified addNumber() to preivous number.

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ *Ming Sun
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNumber;
     
     public GitDemo() {
         previousNumber = 1;
@@ -14,7 +15,8 @@ public class GitDemo {
     *****/
     public double addNumber(double num) {
         System.out.print("Add num to previousNumber");
-        return 0;
+        previousNumber = previousNumber + num;
+        return previousNumber;
     }
     
     /*****
@@ -40,6 +42,7 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
+        reverseNum = newNumber;
         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
